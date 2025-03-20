2ab4bac1cf4cb265f96f878793b42175c6a369c7
syk53111@nzero.kr <dudrhkd531!@>
2025-03-20 09:02:05 +0900

    비트연산자, 조건문

--- src/bagic/bagic1064.java
+++ src/bagic/bagic1064.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class bagic1064 {
     public static void main(String[] args) {
         /*
+
             입력된 세 정수 a, b, c 중 가장 작은 값을 출력하는 프로그램을 작성해보자.
             단, 조건문을 사용하지 않고 3항 연산자 ? 를 사용한다.
 
